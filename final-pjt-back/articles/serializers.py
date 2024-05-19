65f333441e57e2e169ba9b370d46e6ae9595680d
hojun <didl95@naver.com>
2024-05-19 18:07:37 +0900

    _

--- final-pjt-back/articles/serializers.py
+++ final-pjt-back/articles/serializers.py
@@ -3,6 +3,7 @@ from .models import Article, Comment
 
 # 게시글 전체 목록 시리얼라이저
 class ArticleListSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(source='user.id', read_only=True)
     username = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
@@ -14,19 +15,21 @@ class ArticleListSerializer(serializers.ModelSerializer):
 # 댓글 시리얼 라이저
 class CommentSerializer(serializers.ModelSerializer):
     article_id = serializers.ReadOnlyField(source='article.id')
+    user_id = serializers.IntegerField(source='user.id', read_only=True)
     username = serializers.ReadOnlyField(source='user.username')
-
+    
     class Meta:
         model = Comment
-        fields = ('id', 'username', 'content', 'created_at', 'updated_at', 'article_id', )
+        fields = ('id', 'username', 'content', 'created_at', 'updated_at', 'article_id', 'user_id', )
 
 
 # 게시글 시리얼라이저
 class ArticleSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True)
     comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+    user_id = serializers.IntegerField(source='user.id', read_only=True)
     username = serializers.CharField(source='user.username', read_only=True)
-
+    
     class Meta:
         model = Article
         fields = '__all__'
